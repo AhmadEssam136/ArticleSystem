2c9814f63068f071f634b99b2024f91d3ad217ce
AhmedEssam <Ahmedessam1361995@gmail.com>
2018-07-21 01:29:19 +0200

    new home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,39 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                        <form class="form-horizontal">
+                            <fieldset>
 
-                    You are logged in!
+                                <!-- Form Name -->
+                                <legend>Article Form</legend>
+
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="article_title">Title</label>
+                                    <div class="col-md-4">
+                                        <input id="article_title" name="article_title" type="text" placeholder="article title" class="form-control input-md">
+
+                                    </div>
+                                </div>
+
+                                <!-- File Button -->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="source_image">Image</label>
+                                    <div class="col-md-4">
+                                        <input id="source_image" name="source_image" class="input-file" type="file">
+                                    </div>
+                                </div>
+
+                                <!-- Textarea -->
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="source_description">Description</label>
+                                    <div class="col-md-4">
+                                        <textarea class="form-control" id="source_description" name="source_description"></textarea>
+                                    </div>
+                                </div>
+
+                            </fieldset>
+                        </form>
+
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
@@ -53,6 +54,7 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
@@ -64,7 +66,6 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
                         @endguest
                     </ul>
                 </div>
